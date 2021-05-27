33cfb4c9bbe6d61500fab2ea80d3059da6accc0d
Gabriela.Viana12 <gabriela.viana@cs2consultoria.com.br>
2021-05-26 23:30:39 -0300

    implementação do SplashScreen

--- lib/Screens/FirstScreen.dart
+++ lib/Screens/FirstScreen.dart
@@ -1,8 +1,8 @@
-import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 class FirstScreen extends StatefulWidget {
    FirstScreen({Key key}) : super(key: key);
@@ -17,26 +17,23 @@ class _FirstScreenState extends State<FirstScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: GestureDetector(
-          onTap: () async{
-            Future.delayed(Duration(seconds: 5)
-            );
-            Navigator.push(context, MaterialPageRoute(builder: (__) => SecondView()));
-          },
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  "assets/PetControlLogo.svg",
-                  height: MediaQuery.of(context).size.height * 0.7,
-                  color: Colors.grey.shade800,
-                ),
-              ],
-            ),
-          ),
-        ),
+      body: Stack(
+         children:[
+           SplashScreen(
+               seconds: 6,
+               navigateAfterSeconds: SecondView(),
+             useLoader: true,
+             loaderColor: Colors.grey),
+           Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+               SvgPicture.asset(
+                 "assets/PetControlLogo.svg",
+                 height: MediaQuery.of(context).size.height * 0.7,
+                 color: Colors.grey.shade800,
+               ),
+             ],
+           ),],
       ),
     );
   }
@@ -56,7 +53,7 @@ class SecondView extends StatelessWidget{
               height: MediaQuery.of(context).size.height * 0.4,
               color: Colors.grey.shade800,
             ),
-            SizedBox(height: 60),
+            SizedBox(height: 80),
             GestureDetector(
               onTap: () {},
               child: Container(
